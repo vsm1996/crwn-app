392a6a7ccaef197999e5a8b792b27094d0724cb0
Vanessa Martin <vmartin@altair.com>
2020-01-24 08:13:52 -0800

    added new styles to sign-in component

--- src/App.js
+++ src/App.js
@@ -14,18 +14,28 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      currenctUser: null
+      currentUser: null
     };
   }
 
+  unsubscribeFromAuth = null;
+
   componentDidMount() {
-     
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
   }
 
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./sign-in.styles.scss";
 
@@ -17,9 +17,9 @@ class SignIn extends Component {
   }
 
   handleChange = event => {
-      const { name, value } = event.target
-      this.setState({[name]: value})
-  }
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
 
   handleSubmit = event => {
     event.preventDefault();
@@ -32,7 +32,7 @@ class SignIn extends Component {
       <div className="sign-in">
         <h2>I already have an account.</h2>
         <span>Sign in with your email and password</span>
-        
+
         <form onSubmit={this.handleSubmit}>
           <FormInput
             type="email"
@@ -50,8 +50,14 @@ class SignIn extends Component {
             handleChange={this.handleChange}
             required
           />
-          <CustomButton type="submit"> Sign in </CustomButton>
-          <CustomButton onClick={signInWithGoogle}> Sign in with Google</CustomButton>
+
+          <div className="buttons">
+            <CustomButton type="submit"> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              {" "}
+              Sign in with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
