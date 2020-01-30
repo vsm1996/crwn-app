bd5ee8af31a24e4cf3d9ea949ddc9bbfa88721f0
Vanessa Martin <vmartin@altair.com>
2020-01-29 16:22:26 -0800

    included signin/signup auth with email and password | data stored in firestore

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./sign-in.styles.scss";
 
@@ -21,10 +21,16 @@ class SignIn extends Component {
     this.setState({ [name]: value });
   };
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state
+    try {
+      await auth.signInWithEmailAndPassword( email, password)
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.error(error)
+    }
   };
   render() {
     const { email, password } = this.state;
